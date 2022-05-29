58de08f884e183c29fbf6e62c09c5280cf8b982d
Adbinksou <91962946+Adbinksou@users.noreply.github.com>
2022-05-29 13:38:41 +0200

    test +

--- src/test/java/com/example/rpg_final/JeuTest.java
+++ src/test/java/com/example/rpg_final/JeuTest.java
@@ -2,19 +2,42 @@ package com.example.rpg_final;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static com.example.rpg_final.Game2.Jeu;
-
 public class JeuTest {
     @Test
-    public void testHero(){
+    public void testHero() {
         Warrior warrior = new Warrior(100, 20, 5);
-        Assertions.assertEquals(100,warrior.getLifePoints());
+        Assertions.assertEquals(100, warrior.getLifePoints());
 
 
     }
 
     @Test
-    public void testFleche(){
+    public void testVie() {
+        Hunter hunter = new Hunter(60, 25, 6, 4);
+        Enemy enemy = new Enemy(100, 15);
+        boolean condition = false;
+        if (enemy.getLifePoints() > 0 && hunter.getLifePoints() > 0) {
+            condition = true;
+
+        }
+
+
+        Assertions.assertTrue(condition);
+
 
     }
+
+    @Test
+    public void testDamage(){
+        Mage mage = new Mage(75, 25, 0, 20, 5);
+        Enemy enemy = new Enemy(100, 15);
+
+        int lifePointsExpected = 100 - 25;
+        enemy.setLifePoints(enemy.getLifePoints() - mage.getWeaponDamage());
+
+        Assertions.assertEquals(lifePointsExpected,enemy.getLifePoints());
+    }
+
+
+
 }
